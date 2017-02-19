0e1bf55db866fa6166e976a288d09e1e8d2a06ae
SvitlanaT <svitlana.tarapata@gmail.com>
2017-02-19 15:27:47 +0200

    ghg

--- src/homework/lesson2/Worker1.java
+++ src/homework/lesson2/Worker1.java
@@ -4,6 +4,7 @@ package homework.lesson2;
  * @author Света on 19.02.2017.
  */
 public class Worker1 extends Boss {
+    @Override
     public void hobby(){
         System.out.println("I'm running");
     }
